e2d08e66bd690b5dc8cc1f5ed50ea502e2fa6f31
Bhavyai Gupta <zbhavyai@gmail.com>
2021-09-13 18:41:35 -0600

    Added code for accomodating offering

--- src/registrationSystem/CourseCatalog.java
+++ src/registrationSystem/CourseCatalog.java
@@ -24,12 +24,14 @@ public class CourseCatalog {
 
         try {
             BufferedReader catalogFile = new BufferedReader(new FileReader("course_catalog.txt"));
+            BufferedReader offeringFile;
 
-            String str = "";
+            String catalogStr = "";
+            String offeringStr = "";
 
-            while ((str = catalogFile.readLine()) != null) {
+            while ((catalogStr = catalogFile.readLine()) != null) {
                 // 0 = course, 1..* course pre-requisite
-                String[] courses = str.split(",");
+                String[] courses = catalogStr.split(",");
 
                 // create the object of the course
                 Course theCourse = new Course(courses[0].split("-")[0].trim(), courses[0].split("-")[1].trim());
@@ -42,18 +44,48 @@ public class CourseCatalog {
                     preReqList.add(new Course(courses[i].split("-")[0].trim(), courses[i].split("-")[1].trim()));
                 }
 
+                // create the ArrayList of course offerings
+                ArrayList<Offering> offeringList = new ArrayList<>();
+
+                // add the course offerings to the course offerings list
+                offeringFile = new BufferedReader(new FileReader("course_offerings.txt"));
+
+                while ((offeringStr = offeringFile.readLine()) != null) {
+                    String[] offering = offeringStr.split(",");
+
+                    // check if offering line read matches the current course line read
+                    if(theCourse.equals(new Course(offering[0].split("-")[0].trim(), offering[0].split("-")[1].trim()))) {
+                        Offering theOffering = new Offering(Integer.parseInt(offering[1].trim()), Integer.parseInt(offering[2].trim()));
+                        theOffering.setTheCourse(theCourse);
+
+                        offeringList.add(theOffering);
+                    }
+
+                    else {
+                        continue;
+                    }
+                }
+
+                offeringFile.close();
+
                 // attach the pre-requisite list to the course
                 theCourse.setPreReq(preReqList);
 
+                // attach the offering list to the course
+                theCourse.setOfferingList(offeringList);
+
                 // add the course (along with its pre-requisite) to the catalog
                 allCourses.add(theCourse);
+
+                // System.out.printf("%s Section-%d Capacity-%d%n", theCourse, offeringList.get(0).getSectionNumber(), offeringList.get(0).getSectionCapacity());
             }
 
             catalogFile.close();
         }
 
         catch (FileNotFoundException e) {
-            System.err.printf("%n[FAIL] Course Catalog not found. Cannot proceed further. Bye!%n%n");
+            System.err.printf("%n[FAIL] File not found. Cannot proceed further. Bye!%n%n");
+            e.printStackTrace();
             Runtime.getRuntime().exit(1);
         }
 
@@ -84,7 +116,7 @@ public class CourseCatalog {
         StringBuilder sb = new StringBuilder();
 
         for (Course c : courseList) {
-            sb.append(String.format("%s%n", c.toStringCourseAndPreReq()));
+            sb.append(String.format("%s%n", c.toStringCourseAndOffering()));
         }
 
         return sb.toString();
